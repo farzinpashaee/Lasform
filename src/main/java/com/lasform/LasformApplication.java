ef7f02d9fab708ee009ac442080b45f4726bc517
fpashaee <f.pashaee@yahoo.com>
2018-07-08 16:12:56 +0430

    Project initiation

---
+++ src/main/java/com/lasform/LasformApplication.java
@@ -0,0 +1,12 @@
+package com.lasform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LasformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LasformApplication.class, args);
+    }
+}
