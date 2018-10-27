3ffeacdb270b1347c1b1bbcf0875def18620c5a6
farzinpashaee <f.pashaee@yahoo.com>
2018-10-28 00:01:16 +0330

    UPDATE: adding angular

--- src/main/resources/public/scripts/main.js
+++ src/main/resources/public/scripts/main.js
@@ -1,18 +1,39 @@
 requirejs.config({
-    baseUrl: 'scripts/lib',
+    baseUrl: 'scripts',
     paths: {
-        app: '../app',
-        async: 'requirejs-plugins/async',
+        lfCore: 'app/lasform.core',
+        jquery: 'jquery.js',
+        angular: 'lib/angular/angular.min',
+        angularAnim: 'lib/angular/angular.animate.min',
+        angularAria: 'lib/angular/angular.aria.min',
+        angularMessage: 'lib/angular/angular.messages.min',
+        angularMaterial: 'lib/angular/angular.material.min',
+        async: 'lib/requirejs-plugins/async',
+        googleMap: 'https://maps.googleapis.com/maps/api/js?key=AIzaSyDQz41w41dpAu2o9lPssyUCnDgd4rxGpYA&callback=initPage'
     },
     "shim": {
         "jquery.nicescroll.min": ["jquery"],
+        'angular': {
+            exports: 'angular'
+        }
     }
 });
 
-define(['jquery',
-        'async!https://maps.googleapis.com/maps/api/js?key=AIzaSyDQz41w41dpAu2o9lPssyUCnDgd4rxGpYA&callback=initPage',
-        'app/lasform.core'],function ($,gm,lastform) {
 
-    lastform.prepare();
+define(['angular'],function(angular){
 
-});
+    angular.module('lfApp', [])
+        .controller('appCtrl', function($scope) {
+            $scope.title1 = 'Button';
+        });
+
+    //lastform.prepare();
+})
+
+// define(['jquery',
+//         'async!https://maps.googleapis.com/maps/api/js?key=AIzaSyDQz41w41dpAu2o9lPssyUCnDgd4rxGpYA&callback=initPage',
+//         'app/lasform.core'],function ($,gm,lastform) {
+//
+//     lastform.prepare();
+//
+// });
