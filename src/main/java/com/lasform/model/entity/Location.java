1edf31882433d50195679cce43ae13ff67c4014a
farzinpashaee <f.pashaee@yahoo.com>
2019-04-03 12:16:19 +0430

    UPDATE: update location entity

--- src/main/java/com/lasform/model/entity/Location.java
+++ src/main/java/com/lasform/model/entity/Location.java
@@ -1,20 +1,28 @@
 package com.lasform.model.entity;
 
+import lombok.Data;
 import org.springframework.lang.Nullable;
 
 import javax.persistence.*;
 
 @Entity
+@Data
 public class Location {
 
     @Id
     @GeneratedValue(strategy=GenerationType.AUTO)
     private long id;
+    @Column(length=15)
     private String latitude;
+    @Column(length=15)
     private String longitude;
+    @Column(length=80)
     private String name;
+    @Column(length=200)
     private String description;
+    @Column(length=150)
     private String address;
+    @Column(length=200)
     private String additionalData;
     private boolean cover;
     private short imageSlide = 0;
@@ -26,104 +34,9 @@ public class Location {
     @JoinColumn(name = "LOCATION_TYPE_ID")
     private LocationType locationType;
     @OneToOne
-    @Nullable
     @JoinColumn(name = "LOCATION_GROUP_ID")
+    @Nullable
     private LocationGroup locationGroup;
 
 
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getLatitude() {
-        return latitude;
-    }
-
-    public void setLatitude(String latitude) {
-        this.latitude = latitude;
-    }
-
-    public String getLongitude() {
-        return longitude;
-    }
-
-    public void setLongitude(String longitude) {
-        this.longitude = longitude;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public City getCity() {
-        return city;
-    }
-
-    public void setCity(City city) {
-        this.city = city;
-    }
-
-    public LocationType getLocationType() {
-        return locationType;
-    }
-
-    public void setLocationType(LocationType locationType) {
-        this.locationType = locationType;
-    }
-
-    public String getAdditionalData() {
-        return additionalData;
-    }
-
-    public void setAdditionalData(String additionalData) {
-        this.additionalData = additionalData;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public boolean isCover() {
-        return cover;
-    }
-
-    public void setCover(boolean cover) {
-        this.cover = cover;
-    }
-
-    public short getImageSlide() {
-        return imageSlide;
-    }
-
-    public void setImageSlide(short imageSlide) {
-        this.imageSlide = imageSlide;
-    }
-
-    public short getRating() {
-        return rating;
-    }
-
-    public void setRating(short rating) {
-        this.rating = rating;
-    }
 }
