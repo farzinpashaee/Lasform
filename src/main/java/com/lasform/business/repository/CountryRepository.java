4f03b5b01244f590ecfea80ba40099feec8f0fb0
farzinpashaee <f.pashaee@yahoo.com>
2019-04-11 23:38:39 +0430

    FIX: repository annotation fix

--- src/main/java/com/lasform/business/repository/CountryRepository.java
+++ src/main/java/com/lasform/business/repository/CountryRepository.java
@@ -2,6 +2,7 @@ package com.lasform.business.repository;
 
 import com.lasform.model.entity.Country;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
