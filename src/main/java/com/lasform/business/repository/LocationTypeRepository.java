a2d8f30a7699ac5838cd8890352962af134c76c0
Farzin Pashaee <f.pashaee@yahoo.com>
2019-04-19 15:08:32 +0430

    UPDATE : document folder added

--- src/main/java/com/lasform/business/repository/LocationTypeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.lasform.business.repository;
-
-import com.lasform.model.entity.LocationType;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface LocationTypeRepository extends JpaRepository<LocationType,Long> {
-}
