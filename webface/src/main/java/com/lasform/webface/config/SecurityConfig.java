c4fc1e4a0dbf0ab8f5030e0ba1482ff16ee0e8f7
farzinpashaee <f.pashaee@yahoo.com>
2019-05-28 00:03:10 +0430

    FIX: Security config for webface module

---
+++ webface/src/main/java/com/lasform/webface/config/SecurityConfig.java
@@ -0,0 +1,28 @@
+package com.lasform.webface.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/**").permitAll()
+                .antMatchers("/manage/**").hasRole("USER")
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .permitAll()
+                .and()
+                .logout()
+                .permitAll();
+        http.csrf().disable();
+    }
+
+
+}
