e13b13e04e4da5728ff30561605f80f98481b8b7
farzinpashaee <f.pashaee@yahoo.com>
2019-06-17 13:20:04 +0430

    ADD: PathLocation added to the project

--- core/src/main/java/com/lasform/core/model/entity/Path.java
+++ core/src/main/java/com/lasform/core/model/entity/Path.java
@@ -1,11 +1,12 @@
 package com.lasform.core.model.entity;
 
 import lombok.Data;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.Date;
+import java.util.List;
 
 @Data
 @Entity
@@ -16,6 +17,12 @@ public class Path {
     private long id;
     private String name;
     private String description;
-    private String pathLocations;
+    @OneToMany
+    @JoinColumn(name = "PATH_LOCATIONS")
+    private List<PathLocation> pathLocations;
+    @CreatedDate
+    Date createDate;
+    @LastModifiedDate
+    Date modifiedDate;
 
 }
