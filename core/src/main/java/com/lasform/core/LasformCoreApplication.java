246b7b119dea7ed9e04f3393e9d8e9b0a3e4ffbe
farzinpashaee <f.pashaee@yahoo.com>
2019-07-12 22:16:09 +0430

    FIX: Fix data view

--- core/src/main/java/com/lasform/core/LasformCoreApplication.java
+++ core/src/main/java/com/lasform/core/LasformCoreApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
@@ -12,6 +13,7 @@ import java.util.Locale;
 
 @SpringBootApplication
 @EnableWebSecurity
+@EnableJpaAuditing
 public class LasformCoreApplication {
 
     public static void main(String[] args) {
