9931fd63a227d4df318e49b5ad6da1f1e00fc78b
farzinpashaee <f.pashaee@yahoo.com>
2020-03-13 17:30:19 +0800

    UPDATE: update socket service properties

---
+++ core/src/main/java/com/lasform/core/config/properties/SocketServiceProperties.java
@@ -0,0 +1,24 @@
+package com.lasform.core.config.properties;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+import lombok.Data;
+
+@Data
+@Configuration
+@PropertySource("classpath:application.properties")
+@ConfigurationProperties(prefix = "lasform.socket-service")
+public class SocketServiceProperties {
+	
+	private int port = 8089;
+	private int timeout = 2000;
+	
+	private int threadPoolCorePoolSize = 50 ;
+	private int threadPoolMaxPoolSize = 300 ;
+	private int threadPoolQueueCapacity = 50 ;
+	
+	
+
+}
